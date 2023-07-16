e8478da5a275989a84521e5c9f21b323ab460703
nanes03 <inesmoalla68@gmail.com>
2023-07-16 10:33:56 +0100

    new update

--- add_admin.php
+++ add_admin.php
@@ -8,9 +8,10 @@ if(!isset($_SESSION))
 { 
     session_start(); 
 }
+if(isset ($id) ){
 
 
-$id=mysqli_real_escape_string($con,$_POST['id']);
+$id=mysqli_real_escape_string($con,$_POST["id"]);
 
 if(isset($_POST['add_admin_btn'])){
     $query="UPDATE `lrsetitmembre` SET `etat`='1' WHERE `id`='$id' AND `approve`='1'";
@@ -18,9 +19,9 @@ if(isset($_POST['add_admin_btn'])){
     if(mysqli_query($con,$query) ){
 
         echo 200;
-    }else{
+    }else{  
         echo 500;
     }
 }
-
+}
 ?>
--- evenements.php
+++ evenements.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Lab-SETIT</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
@@ -37,7 +37,7 @@
       }
     </style>
   <script>
-    $(document).ready(function(){
+    $(Lab-SETIT).ready(function(){
       $(".dropdown").hover(function(){
         var dropdownMenu = $(this).children(".dropdown-menu");
         if(dropdownMenu.is(":visible")){
@@ -72,7 +72,7 @@ if(!isset($_SESSION))
 
 require_once ("password-connection.php");  
 $query="SELECT * FROM `conference_manifestation` " ;
-$result=mysqli_query($con,$query);
+$result_conference_manifestation=mysqli_query($con,$query_conference_manifestation);
 
 $query_formation="SELECT * FROM `formation` " ;
 $result_formation=mysqli_query($con,$query_formation);
